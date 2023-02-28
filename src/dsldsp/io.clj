bbf6521b681932f0b8155fdc825ce7536492b64e
bestestest kitteh <236551@edu.p.lodz.pl>
2023-02-28 18:50:31 +0100

    update schema to allow complex numbers

--- src/dsldsp/io.clj
+++ src/dsldsp/io.clj
@@ -4,15 +4,23 @@
             [clojure.java.io :as io])
   (:import (java.nio ByteBuffer)))
 
-(def binary-schema (g/compile-frame {:type :discrete
-                                     :start :int64
-                                     :duration :int64
-                                     :sampling :float64
-                                     :values (g/repeated :float64)}))
+(def raw {:type :discrete
+          :start :int64
+          :duration :int64
+          :sampling :float64
+          :values (g/repeated :float64)})
+(g/defcodec is-complex (g/enum :byte :plain :complex))
+(g/defcodec plain raw)
+(g/defcodec complex (assoc raw :complex (g/repeated :float64)))
+(g/defcodec binary-schema
+  (g/header
+   is-complex
+   {:complex complex :plain plain}
+   #(if (:complex %) :complex :plain)))
 
 (defn write [f discrete]
-  (with-open [fs (io/output-stream discrete)]
-    (i/encode-to-stream binary-schema fs [f])))
+  (with-open [fs (io/output-stream f)]
+    (i/encode-to-stream binary-schema fs [discrete])))
 
 (defn- file->bb [f]
   (with-open [fs (io/input-stream f)
