65a96e40a03b70cd993678b98b82e2ac04f1e23a
bestestest kitteh <236551@edu.p.lodz.pl>
2023-02-28 13:05:15 +0100

    ..

--- src/dsldsp/io.clj
+++ src/dsldsp/io.clj
@@ -4,21 +4,24 @@
             [clojure.java.io :as io])
   (:import (java.nio ByteBuffer)))
 
-(def ex {:type :discrete :sampling (/ 1000) :start 0 :duration 10
-         :values (into [] (concat (repeat 2 0)
-                                  (repeat 6 0.5)
-                                  (repeat 2 1)))})
-(def b (g/compile-frame {:type :discrete
-                         :start :int64
-                         :duration :int64
-                         :sampling :float64
-                         :values (g/repeated :float64)}))
+(def binary-schema (g/compile-frame {:type :discrete
+                                     :start :int64
+                                     :duration :int64
+                                     :sampling :float64
+                                     :values (g/repeated :float64)}))
+
 (defn write [f discrete]
   (with-open [fs (io/output-stream discrete)]
-    (i/encode-to-stream b fs [f])))
+    (i/encode-to-stream binary-schema fs [f])))
 
-(defn read [f]
+(defn- file->bb [f]
   (with-open [fs (io/input-stream f)
               out (java.io.ByteArrayOutputStream.)]
     (io/copy fs out)
-    (i/decode b (ByteBuffer/wrap (.toByteArray out)))))
+    (.toByteArray out)))
+
+(defn read [f]
+  (->> f
+       file->bb
+       ByteBuffer/wrap
+       (i/decode binary-schema)))
