f836e71365331e099c22d09f00927eccc9a8dfc3
bestestest kitteh <236551@edu.p.lodz.pl>
2023-03-02 14:45:36 +0100

    neater examples

--- src/dsldsp/core.clj
+++ src/dsldsp/core.clj
@@ -4,6 +4,7 @@
             [dsldsp.io :as i]))
 ;; fancy are a second-class citizens, everything complex is done via discrete
 (comment
+
   ;; fourier thingy demo
   (g/show
    (apply s/fop +
@@ -19,22 +20,22 @@
           {:function :sin}))
 
   (g/show
-   (s/fop +
+   (s/dop +
           {:function :triangle :fill 0.0 :start 0 :duration 2}
           {:function :triangle :fill 0.5 :start 4 :duration 2}
           {:function :triangle :fill 1.0 :start 8 :duration 2}))
 
-  (g/showboth (apply s/dop + (for [x (range 500)] (s/impulse-noise :p 0.5))))
-  (g/showboth (apply s/fop + (for [x (range 500)] {:function :noise})))
-  (do
-    (i/write "uwu" {:function :jump
-                    :start 10
-                    :fill 12
-                    :duration 10})
-    (g/show (s/fancy->discrete {:function :jump
-                                :start 10
-                                :fill 12
-                                :duration 10}))
-    (g/show (i/read "uwu"))))
+  (g/showboth (apply s/dop + (for [x (range 50)] (s/impulse-noise :p 0.5))))
+
+  (let [uwu {:function :jump
+             :start 10
+             :fill 12
+             :duration 10}]
+    (i/write "uwu" uwu)
+    (g/show uwu)
+    (g/show (i/read "uwu")))
+
+  (g/histogram {:function :square :duration 1.5})
+  (g/stat {:function :triangle :duration 22.1}))
 
 (keys s/functions-cont)
