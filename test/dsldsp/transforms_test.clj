d251454f072caadaf1efc820de350b18d5991670
bestestest kitteh <236551@edu.p.lodz.pl>
2023-06-01 16:36:56 +0200

    meow!

--- test/dsldsp/transforms_test.clj
+++ test/dsldsp/transforms_test.clj
@@ -1,7 +1,15 @@
 (ns dsldsp.transforms-test
   (:require [dsldsp.transforms :as sut]
-            [clojure.test :as t]))
+            [clojure.test :as t]
+            [complex.core :as c]))
 
+(def ex-arr16 (object-array (map c/complex (range 16))))
+(def ex-arr64 (object-array (map c/complex (range 64))))
+(defn diff [arr normal fast]
+  (let [a (normal arr)
+        b (fast arr)]
+    diff (->> (map #(.abs (c/- %1 %2)) a b)
+              (reduce +))))
 (t/deftest reversing
   (t/are [b a] (= [a b] [(sut/rev-int 3 b) b])
     0 0
@@ -22,3 +30,6 @@
     6 6
     7 14
     8 1))
+(t/deftest fft
+  (t/is (> 1e-5 (diff ex-arr16 sut/F-1 (sut/fft-w-miejscu-czas -))))
+  (t/is (> 1e-5 (diff ex-arr64 sut/F-1 (sut/fft-w-miejscu-czas -)))))
