a2dd51abd992b3952735c5c8b46e2961d9bfce91
bestestest kitteh <236551@edu.p.lodz.pl>
2023-06-01 17:34:59 +0200

    clean1

--- test/dsldsp/transforms_test.clj
+++ test/dsldsp/transforms_test.clj
@@ -6,12 +6,15 @@
             [complex.core :as c]))
 
 (def ex-arr16 (object-array (map c/complex (range 16))))
+
 (def ex-arr64 (object-array (map c/complex (range 64))))
+
 (defn diff [arr normal fast]
   (let [a (normal arr)
         b (fast arr)]
     (->> (map #(.abs (c/- %1 %2)) a b)
          (reduce +))))
+
 (t/deftest reversing
   (t/are [b a] (= [a b] [(sut/rev-int 3 b) b])
     0 0
@@ -32,14 +35,17 @@
     6 6
     7 14
     8 1))
+
 (t/deftest fft
   (t/is (> 1e-5 (diff ex-arr16 (sut/fft-slow -) (sut/fft-w-miejscu-czas -))))
   (t/is (> 1e-5 (diff ex-arr64 (sut/fft-slow -) (sut/fft-w-miejscu-czas -))))
   (t/is (> 1e-5 (diff ex-arr16 (sut/fft-slow +) (sut/fft-w-miejscu-czas +))))
   (t/is (> 1e-5 (diff ex-arr64 (sut/fft-slow +) (sut/fft-w-miejscu-czas +)))))
+
 (def test-sig (s/fop +
                      {:fun :sin :period 0.128 :end 0.128}
                      {:fun :sin :period 0.064 :phase 0.5 :end 0.128}
                      {:fun :sin :period 0.032 :phase 0.25 :end 0.128}))
+
 (comment
-  (g/show (sut/przebrandzluj (sut/fft-w-miejscu-częstotliwość -) test-sig)))
+  (binding [g/*together* false g/*magnitude* false] (g/show (sut/przebrandzluj (sut/fft-w-miejscu-częstotliwość -) test-sig))))
