35956e281e0f928dd185e031a29abbd0b832bea2
bestestest kitteh <236551@edu.p.lodz.pl>
2023-06-11 17:04:41 +0200

    ...

--- src/dsldsp/core.clj
+++ src/dsldsp/core.clj
@@ -5,17 +5,12 @@
             [dsldsp.filter :as f :refer :all]
             [complex.core :as c]))
 
-(defn showf [a b] (graph (make-complex (fancy a) (fancy b))))
-(defn showw [a b] (graph (make-complex a b)))
 (comment
-
-  (show
-   (apply fop +
-          (for [x (range 2 3000 4)]
-            {:function :sin
-             :duration 1
-             :period (/ x)
-             :amplitude (/ x)})))
+  ;; pokaż funkcję
+  (show {:fun :sin})
+  ;; dodaj funkcje
+  (show (fop + {:fun :sin} {:fun :sin :A 1/5 :period 1/5}))
+  ;; współpraca z językiem
   (show
    (apply fop +
           (for [x (range 2 300 4)]
@@ -23,63 +18,57 @@
              :duration 1
              :period (/ x)
              :amplitude (/ x)})))
-  ;; fourier thingy demo
 
-  (binding [hist-bins 20
-            sampling-period 1/73]
-    (show
-     (make-complex (apply dop max
-                          (for [x (range 0 1 (/ 20))]
-                            {:function :sin :phase x}))
-                   (dop max
-                        {:function :sin :phase 1/2}
-                        {:function :sin}))))
+  ;; pokazywanie wielu funkcji jednocześnie, inne operatory, zmiana częstotliwości próbkowania
+  (binding [sampling-period 1/730]
+    (show-two
+     (apply dop max
+            (for [x (range 0 1 (/ 5))]
+              {:function :sin :phase x}))
+     (dop max
+          {:function :sin :phase 1/2}
+          {:function :sin})))
 
+  ;; więcej opcji dot. sygnałów
   (show
-   (dop +
-        {:function :triangle :fill 0.0 :start 0 :duration 2}
+   (dop -
         {:fun :triangle :fill 0.5 :s 4 :d 2}
+        {:function :triangle :fill 0.0 :start 0 :duration 2}
         {:fun :triangle :fill 1.0 :s 8 :e 10}))
 
+  ;; obliczanie statystyk
   (binding [sampling-period 0.05] (stat {:fun :sin}))
 
-  (binding [graph-samples 10000] (show
-                                  ;; UwU
-                                  (let [v (fop -
-                                               {:fun :const :e 1}
-                                               {:fun :sin-half :period 2 :e 1})
-                                        U (fop * v v v)
-                                        w (tshift {:fun :triangle :phase 0.5 :period 1/2 :len 1} 2.5)]
-                                    (dop max
-                                         (tshift U 1.5)
-                                         (tshift U 3.5)
-                                         (fop #(* % 1/3 (Math/sqrt %)) w)))))
+
+  ;; zapisywanie do pliku
+  (let [v (fop -
+               {:fun :const :e 1}
+               {:fun :sin-half :period 2 :e 1})
+        U (fop * v v v)
+        w (tshift {:fun :triangle :phase 0.5 :period 1/2 :len 1} 2.5)
+        f (dop max
+               (tshift U 1.5)
+               (tshift U 3.5)
+               (fop #(* % 1/3 (Math/sqrt %)) w))]
+    (write "UwU" f))
+
+  ;; odczytywanie z pliku
   (show "UwU")
 
+  ;; histogram
   (histogram
    (apply dop +
-          (for [_ (range 500)] {:fun :noise-impulse :fill 0.5})))
+          (for [_ (range 10)] {:fun :noise-impulse :fill 0.5})))
 
-  (binding [sampling-period 1/500]
+  ;; liczby złożone
+  (histogram (make-complex {:fun :noise-gauss} {:fun :sin}))
+
+  ;; impulsy
+  (binding [sampling-period 1/100]
     (show (dop + {:function :sin :duration 0.1  :amplitude 3}
                (impulse :ns 2)
                (impulse :ns 4))))
 
-  (write "sig" {:f :sin :period 1/100 :duration 1/10})
-  (show "sig")
-  (binding [sampling-period 1/10000]
-    (write "sig2" {:period 1/20 :f :sin :duration 1/10 :A 5}))
-  (show (dop + "sig" "sig2"))
-  (show (discrete {:fun :noise-gauss}))
-  (show {:fun :noise :A 3.5})
-  (show {:function :square :duration 1.5})
-
-  (stat {:function :triangle :duration 22.1})
-
-  (let [sig (make-complex {:f :sin}
-                          {:f :sin :ph 1/2})]
-    (show (dop c/* sig sig)))
-  ;;
   ;; kwantyzacja
   (let [x (fop + {:f :sin} {:f :sin :A 1/2 :p 1/2})]
     (show (make-complex (fop (kwant 1/5) x)
@@ -88,26 +77,21 @@
   ;; porównanie metod
   (binding [sampling-period 1/10]
     (let [x (fop + {:f :sin} {:f :sin :A 1/2 :p 1/2} {:f :sin :p 3})]
-      (show (make-complex x
-                          (rzędu-pierwszego (discrete x))))
-      (show (make-complex x
-                          (rzędu-zerowego (discrete x))))))
+      (show-two x (rzędu-pierwszego (discrete x)))
+      (show-two x (rzędu-zerowego (discrete x)))
+      (show-two x (sinc-1 (discrete x) 5))))
 
-  (let [x {:f :sin :s -5 :e 5}
-        n 2.6]
-    (show (make-complex
-           (sinc-0
-            (binding [sampling-period (/ n)]
-              (discrete x)) (int (* n 3)))
-           x)))
+  ;; numeryczne porównanie
   (binding [sampling-period 1/200]
     (let [x {:f :sin :s -5 :e 5}
           sampled (binding [sampling-period 1/6] (discrete x))
           x (discrete x)]
       {:0 (other-stat x (rzędu-zerowego sampled))
        :1 (other-stat x (rzędu-pierwszego sampled))
-       :sinc (other-stat x (sinc-0 sampled 35))}))
+       :sinc35 (other-stat x (sinc-1 sampled 35))
+       :sinc5 (other-stat x (sinc-1 sampled 5))}))
 
+  ;; wszystkie funkcje
   (->> (for [[i f] (map-indexed vector [{:f :sin}
                                         {:f :sin-half}
                                         {:f :sin-double}
@@ -123,62 +107,37 @@
                                         {:f :noise-impulse}])]
          (assoc f :start (* i 3) :duration 2))
        (apply fop + {:f :jump :fill 23 :s 0 :e 39 :a 4})
-       show)
-
-  (binding [sampling-period 1/100]
-    (let [z (fop +  {:f :sin :spread true}
-                 {:f :sin :spread true :A 0.5 :period 1/5}
-                 {:f :noise :A 0.1 :spread true})
-          x z
-          n 30
-          i1 (impulse :A (/ n) :len n :ns (- (/ n 2)))
-          i2 (make-window n)]
-      (showw z z))))
+       show))
 
 (comment
   ;; showcase filters
-  (showf
+  (show-two
    (filter-stat 1/128 1 (make-filter {:M 32 :K 8 :pass upper :window blackman}))
    (filter-stat 1/128 1 (make-filter {:M 64 :K 8 :pass lower})))
-  (showf
+  (show-two
    (filter-stat 1/256 1 (make-filter {:M 64 :K 8 :pass middle :window hanning}))
    (filter-stat 1/256 1 (make-filter {:M 64 :K 8 :pass middle})))
-  (showf
+  (show-two
    (filter-stat 1/256 1 (make-filter {:M 20 :K 8 :pass middle :window hanning}))
    (filter-stat 1/256 1 (make-filter {:M 20 :K 8 :pass middle})))
-  (showf (filter-stat 1/128 1 (convolute (make-filter {:M 40 :K 3 :pass lower :window hanning})
-                                         (make-filter {:M 40 :K 4 :pass middle :window hanning})))
-         (filter-stat 1/128 1 (convolute (make-filter {:M 40 :K 4 :pass lower})
-                                         (make-filter {:M 40 :K 4 :pass middle}))))
-  (showf (filter-stat 1/256 1 (convolute (make-filter {:M 40 :K 2 :pass lower :window hanning})
-                                         (make-filter {:M 40 :K 2 :pass upper})))
-         (filter-stat 1/256 1 (convolute (make-filter {:M 40 :K 4 :pass middle})
-                                         (make-filter {:M 40 :K 4 :pass middle}))))
-  (showf (filter-stat 1/128 1 (make-lower-pass-filter {:M 50} 200))
-         (filter-stat 1/128 1 (make-upper-pass-filter {:M 50} 50)))
-  (showf
+  (show-two (filter-stat 1/128 1 (make-lower-pass-filter {:M 50} 200))
+            (filter-stat 1/128 1 (make-upper-pass-filter {:M 50} 50)))
+  (show-two
    (filter-stat 1/128 1 (make-pass-filter {:M 40 :window hanning} 200 400))
    (filter-stat 1/128 1 (make-pass-filter {:M 100 :window blackman} 100 300)))
+  ;; filtry na praktycznej funkcji
   (binding [sampling-period 1/200]
     (let [z (dop +
                  {:f :sin :period 1/55}
                  {:f :sin :period 1/20}
-                 {:f :sin :period 1/5}
-                 ;; {:f :noise :A 0.1 :spread true}
-                 )
+                 {:f :sin :period 1/5})
           prepare #(cut (sinc-1 % 20) 2.5 2.7)
           compare #(showf (prepare z) (prepare (convolute z %)))]
       (compare (make-lower-pass-filter {:M 80} 10))
       (compare (make-pass-filter {:M 80} 18 22))
       (compare (make-pass-filter {:M 80} 50 60)))))
 
-;; wyższe M po prostu poprawia jakość
-;; K:
-;; dolnoprzepustowy filtr przepuszcza częstotliwości poniżej 1/K dl FP=100Hz dla K=4 będzie przepuszczało sygnały poniżej 25HZ
-;; pasmowy filtr pdrzepuszcza częstotliwości
-;; zawsze przepuszcza FP*4,
-;; okno skaluje się odwrotnie proporcjonalnie z K
-;; {}
+
 (defn- calc-delay [signal response]
   (float (- (max-time (correlate signal signal))
             (max-time (correlate signal response)))))
